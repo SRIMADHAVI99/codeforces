ec2ed387c78ef8bd4e7efb30ef4e00ac93050f12
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-02-20 22:01:59 +0530

    Add Digits Summation program in C++

---
+++ c++/Digits Summation/Digits Summation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long N,M,number,number1;
+    cin>>N>>M;
+    number=N%10; 
+    number1=M%10;
+    cout<<number+number1;
+    return 0;
+}
