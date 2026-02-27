890fd0d1dcdc33461a98a962bc90914bd85b4dbe
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-02-27 21:07:08 +0530

    Add program to check if two strings are equal

---
+++ c++/The Brothers/The Brothers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string s1,s2,f1,f2;
+  cin>>f1>>s1;
+  cin>>f2>>s2;
+  if(s1==s2){
+      cout<<"ARE Brothers";
+  }
+  else{
+      cout<<"NOT";
+  }
+  return 0;
+}
