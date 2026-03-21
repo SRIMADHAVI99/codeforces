aa478e0c66e8e0fb939eab2583b558b26c4de1e9
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-03-21 21:02:41 +0530

    Create Lucky Numbers.cpp

---
+++ c++/Lucky Numbers/Lucky Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,a,b;
+    cin>>n;
+    a=n/10;
+    b=n%10;
+    if((a%b==0)||(b%a==0)) {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
