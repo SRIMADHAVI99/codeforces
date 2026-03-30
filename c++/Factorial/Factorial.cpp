20c345673106cc59517f6a752f599f59c077db4f
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-03-30 19:27:03 +0530

    Implement factorial calculation in Factorial.cpp

---
+++ c++/Factorial/Factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int F;
+    cin >> F;
+    while(F--){
+        int N;
+        cin >> N;
+        long long fact = 1;
+        for(int i = 1; i <= N; i++){
+            fact *= i;
+        }
+        cout << fact << endl;
+    }
+    return 0;
+}
