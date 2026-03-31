db3e7e9dfd8b41208cf494e8f6f147c4c98a9c8f
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-03-31 19:46:33 +0530

    Implement prime number check in One Prime.cpp

---
+++ c++/One Prime/One Prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N == 1) {
+        cout << "NO";
+        return 0;
+    }
+    for (int i = 2; i * i <= N; i++) {
+        if (N % i == 0) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+    return 0;
+}
