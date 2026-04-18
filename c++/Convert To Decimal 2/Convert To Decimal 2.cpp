5220f8e5d4e160a41ba3303bf43d03e6baa766d7
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-04-18 22:00:06 +0530

    Add Convert To Decimal 2 program

---
+++ c++/Convert To Decimal 2/Convert To Decimal 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        int count = 0;
+        while (n > 0)
+        {
+            n = n & (n - 1);
+            count++;
+        }
+        long long result = (1LL << count) - 1;
+        cout << result << endl;
+    }
+    return 0;
+}
