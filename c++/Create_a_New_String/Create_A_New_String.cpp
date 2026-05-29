598d625a543a0d715a667fcc65f36fd6bf4bf8e0
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-05-29 18:16:53 +0530

    Add program to create and display string lengths

---
+++ c++/Create_a_New_String/Create_A_New_String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string S, A;
+    cin >> S >> A;
+    cout << S.length() << " " << A.length() << endl;
+    cout << S << " " << A << endl;
+    return 0;
+}
