387a24dc8484bd96fb5d8c71b25bd40a4229d6dd
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-05-30 21:33:42 +0530

    Add getline example in Let`s_Use_Getline.cpp

---
+++ c++/Let`s_Use_Getline/Let`s_Use_Getline.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    size_t pos = s.find('\\');
+    cout << s.substr(0, pos);
+
+    return 0;
+}
