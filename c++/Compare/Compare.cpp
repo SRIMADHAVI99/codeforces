c02783cd1aa94e73b6db7b99f908a432ef0a00fc
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-05-31 22:08:10 +0530

    Implement string comparison in Compare.cpp

---
+++ c++/Compare/Compare.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string X, Y;
+    cin >> X >> Y;
+
+    if (X < Y)
+        cout << X;
+    else
+        cout << Y;
+
+    return 0;
+}
