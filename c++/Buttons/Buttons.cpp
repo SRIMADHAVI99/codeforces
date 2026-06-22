60c00d7142f0994bdac4efd6588676d5e2229fc2
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-06-22 23:19:46 +0530

    Implement game logic in Buttons.cpp

---
+++ c++/Buttons/Buttons.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        if(a == b){
+            if(c%2 == 0){
+                cout << "Second" << endl;
+            }
+            else{
+                cout << "First" << endl;
+            }
+        }
+        
+        else{
+            if(a < b){
+                cout << "Second" << endl;
+            }
+            else{
+                cout << "First" << endl;
+            }
+        }
+        
+    }
+    return 0;
+}
