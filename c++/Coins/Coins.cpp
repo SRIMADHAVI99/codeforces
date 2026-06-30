0683610afc1798a3bcddbf9ec90db5be0f19f6af
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-06-30 14:55:55 +0530

    Implement coin decision logic in Coins.cpp

---
+++ c++/Coins/Coins.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+
+        if (k % 2 == 0) {
+            if (n % 2 == 0)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        } else {
+            if (n % 2 == 0)
+                cout << "YES\n";
+            else {
+                if (n >= k)
+                    cout << "YES\n";
+                else
+                    cout << "NO\n";
+            }
+        }
+    }
+
+    return 0;
+}
