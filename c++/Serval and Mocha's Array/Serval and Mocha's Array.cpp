2c1fc9be896afd62223faf3f0d70c05e473b0101
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-07-05 11:30:56 +0530

    Add GCD check for pairs in an array
    Implement a program to check if any two numbers in an array have a GCD less than or equal to 2.

---
+++ c++/Serval and Mocha's Array/Serval and Mocha's Array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <numeric> 
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        
+        int n;
+        cin >> n;
+        
+        vector <int> a(n);
+        
+        for(int i = 0; i< n; i++){
+            cin >> a[i];
+        }
+        
+        bool yes = false;
+
+        for(int i = 0; i< n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(gcd(a[i], a[j]) <= 2){
+                    yes = true;
+                    break;
+                }
+            }
+            if(yes){
+                break;
+            } 
+        }
+        
+        if(yes){
+            cout << "YES" << endl;
+        }
+        
+        else{
+           cout << "NO" << endl; 
+        }
+
+    }
+    return 0;
+}
