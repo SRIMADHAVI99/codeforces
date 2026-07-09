bad309856e164abe139626e69d388aee2feefa8f
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-07-09 21:21:34 +0530

    Add main functionality for counting even/odd sequences
    Implement a program to count consecutive even/odd integers in arrays.

---
+++ c++/Everybody Likes Good Arrays!/Everybody Likes Good Arrays!.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+        
+        vector <int> a(n);
+        
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        
+        int count = 1, temp = 0;
+        
+        for(int i = 1; i < n; i++){
+            if((a[i] % 2 ) == (a[i - 1] % 2)){
+                count++;
+            }
+            else{
+                temp = temp + (count - 1);
+                count = 1;
+            }
+        }
+        
+        temp = temp + (count - 1);
+        cout << temp << endl;
+
+    }
+    return 0;
+}
