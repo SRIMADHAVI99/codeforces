d5f20567676eff71ce12016685281b308207bae7
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-07-21 22:48:04 +0530

    Add Balanced Round problem solution in C++
    Implement a solution for the Balanced Round problem that reads multiple test cases, processes input, sorts values, and calculates the maximum number of elements that can be balanced within a given difference.

---
+++ c++/Balanced_Round/Balanced_Round.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        vector <int> a(n);
+        
+        for(int i = 0; i < n; i++){
+           cin >> a[i]; 
+        }
+        
+        sort(a.begin(), a.end());
+        
+        int count = 1, sum = 1;
+        
+        for(int i = 0; i < n - 1; i++){
+            if(a[i + 1] - a[i] <= k){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            
+            sum = max(sum, count);
+        }
+        
+        cout << n - sum << endl;
+    }
+    return 0;
+}
