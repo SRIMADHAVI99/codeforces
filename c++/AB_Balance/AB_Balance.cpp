37087a98206373c97bf37cf6fbd3138253d86d1b
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-08-03 22:24:26 +0530

    Add main function to process string modifications
    Implement main function to read test cases and modify strings based on front and back characters.

---
+++ c++/AB_Balance/AB_Balance.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        if (s.front() != s.back())
+            s.back() = s.front();
+
+        cout << s << endl;
+    }
+
+    return 0;
+}
