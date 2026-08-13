b837edb9c4a7af154ec5e1d763b2c181ce3a055a
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-08-13 21:06:19 +0530

    Add main game logic for counting zeros and ones

---
+++ c++/01_Game/01_Game.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int zero = 0;
+        int one = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0')
+                zero++;
+            else
+                one++;
+        }
+
+        int moves = min(zero, one);
+
+        if (moves % 2 == 1)
+            cout << "DA" << endl;
+        else
+            cout << "NET" << endl;
+    }
+
+    return 0;
+}
