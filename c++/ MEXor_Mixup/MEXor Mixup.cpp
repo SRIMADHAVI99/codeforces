259fe58e9361ab3afc5474a724e0d0a3c9f03d87
SRIMADHAVI99 <kokkusrimadhavi@gmail.com>
2026-09-05 23:41:35 +0530

    Refactor MEXor Mixup to use int instead of long long

--- c++/ MEXor_Mixup/MEXor Mixup.cpp
+++ c++/ MEXor_Mixup/MEXor Mixup.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int findXor(int n) {
+    if (n % 4 == 0)
+        return n;
+    if (n % 4 == 1)
+        return 1;
+    if (n % 4 == 2)
+        return n + 1;
+
+    return 0;
+}
+
 int main() {
     int t;
     cin >> t;
 
     while (t--) {
-        long long a, b;
+        int a, b;
         cin >> a >> b;
 
-        long long x = 0;
-
-        for (int i = 0; i < a; i++) {
-            x ^= i;
-        }
+        int x = findXor(a - 1);
 
         if (x == b) {
             cout << a << endl;
         }
-        else if ((x ^ b) != a) {
-            cout << a + 1 << endl;
+        else if ((x ^ b) == a) {
+            cout << a + 2 << endl;
         }
         else {
-            cout << a + 2 << endl;
+            cout << a + 1 << endl;
         }
     }
 
